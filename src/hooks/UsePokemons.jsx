d563d719b3581f0d924580d103f2060af5484270
Sergi Linares <slinares@omatech.com>
2022-07-01 11:54:39 +0200

    commit of last session 28-06-22 -> fetch pokemons with API using use effect

--- src/hooks/UsePokemons.jsx
+++ src/hooks/UsePokemons.jsx
@@ -1,33 +1,66 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const usePokemons = ({selectedTypes, searchValue, currentPage, pageSize, orderValue, direction}) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${currentPage * pageSize}&limit=${pageSize}`;
-    let pokemons;
+    const [fetchedPokemons, setFetchedPokemons] = useState([]);
+    const [detailedPokemon, setDetailedPokemon] = useState([]);
+    const [pokemonCount, setPokemonCount] = useState(0);
 
-    const fetchPokemonData = (pokemon) => {
-        let fetchedPokemon;
-        useEffect(() => {
 
-            (async () => {
-                const json = await (await fetch(pokemon.url)).json();
-                fetchedPokemon = json.map(p => p);
-            })();
-            return () => {};
+/*     const filterPokemonTypes = (detailedPokemon) => {
+        if (selectedTypes.length) {
+            console.log(selectedTypes, detailedPokemon);
+            detailedPokemon = detailedPokemon.filter(pokemon =>
+                selectedTypes.some(
+                    type => pokemon.types.includes(type))
+            );
+    
+        }
+    } */
 
-        }, []);
+
+    const pokemonDetail = async(pokemon, controller) => {
+        const request = await fetch(pokemon.url, {signal: controller.signal });
+        const result = await request.json();
+        let detailedPokemon = [];
+        if(request.ok){
+            detailedPokemon = { "moves": result.moves, 
+                "image": result.sprites.front_default, 
+                "name": result.name, 
+                "id": result.id, 
+                "number": result.id, 
+                "types": result.types }; 
+            return detailedPokemon;
+                //setDetailedPokemon(detailedPokemon);
+            //filterPokemonTypes(detailedPokemon);
+        }
     }
 
     useEffect(() => {
+        const controller = new AbortController();
 
         (async () => {
-            const json = await (await fetch(url)).json();
-            pokemons = json.results.map(fetchPokemonData);
+            const request = await fetch(url, { signal: controller.signal });
+            const result = await request.json();
+            setPokemonCount(result.count);
+            let pokemons = [];
+            if(request.ok){
+                pokemons = result.results.map(p => {
+                    let a = pokemonDetail(p, controller);
+                    console.log("a", a);
+                });
+                setFetchedPokemons(pokemons);
+            }
         })();
-        return () => {};
 
-    }, []);
+        return () => controller.abort();
 
-    /*let pokemons = json.map(pokemon => ({
+    }, [url]);
+
+    console.log(fetchedPokemons);
+    //console.log(detailedPokemon);
+    //console.log(detailedPokemon);
+    /*let pokemons = detailedPokemon.map(pokemon => ({
         id: pokemon.id,
         image: pokemon.image,
         number: pokemon.id,
@@ -35,8 +68,8 @@ const usePokemons = ({selectedTypes, searchValue, currentPage, pageSize, orderVa
         moves: pokemon.moves,
         types: pokemon.types
     }));*/
-
-    const pokemonCount = pokemons.length;
+    //let pokemon = 
+/*
     if (selectedTypes.length) {
         pokemons = pokemons.filter(pokemon =>
             selectedTypes.some(
@@ -44,6 +77,7 @@ const usePokemons = ({selectedTypes, searchValue, currentPage, pageSize, orderVa
         );
 
     }
+    *
     pokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchValue.toLowerCase()));
     const pageCount = Math.floor(pokemons.length / pageSize);
 
@@ -56,7 +90,8 @@ const usePokemons = ({selectedTypes, searchValue, currentPage, pageSize, orderVa
 
     pokemons = pokemons.slice(pageSize * currentPage, pageSize * currentPage + pageSize)
 
-    return [pokemonCount, pokemons, pageCount];
+    return [pokemonCount, pokemons, pageCount]; */
+    return [0, [], 0];
 }
 
 export default usePokemons;
