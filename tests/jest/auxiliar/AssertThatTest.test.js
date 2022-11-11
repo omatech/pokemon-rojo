38310ae9bb1832da908abe6da91e1c4e322584ad
pilarvi <pvidal@omatech.com>
2022-11-11 14:03:07 +0100

    Test

--- tests/jest/auxiliar/AssertThatTest.test.js
+++ tests/jest/auxiliar/AssertThatTest.test.js
@@ -37,3 +37,139 @@ test('test', () => {
   expect(500).toSatisfy(n => n > 0 || n < 10)
 })
 
+test('assertEquals', () => {
+  const expectedInt = 0
+  const resultInt = 0
+
+  expect(expectedInt).toEqual(resultInt)
+  expect(expectedInt).not.toEqual(1)
+
+  const expectedFloat = 0.0
+  const resultFloat = 0.1
+
+  expect(expectedFloat).toBeCloseTo(resultFloat, 0.1)
+
+  const expectedChar = "0"
+  const resultChar = "0"
+
+  expect(expectedChar).toEqual(resultChar)
+
+  const expectedBoolean = true
+  const resultBoolean = true
+
+  expect(expectedBoolean).toEqual(resultBoolean)
+
+  const expectedX = new X(null)
+  const resutlX = expectedX
+
+  expect(expectedX).toEqual(resutlX)
+})
+
+test('assertTrue', () => {
+  const resultBoolean = true
+  expect(resultBoolean).toBeTrue()
+})
+//
+//    public function testAssertTrue(): void
+//    {
+//        $resultBoolean = (bool)true;
+//        $this->assertTrue($resultBoolean, "should be true");
+//        $this->assertTrue($resultBoolean);
+//        // with assertThat
+//        assertThat($resultBoolean, equalTo(true));
+//    }
+//
+//    public function testAssertFalse(): void
+//    {
+//        $resultBoolean = (bool)false;
+//        $this->assertFalse($resultBoolean, "should be true");
+//        $this->assertFalse($resultBoolean);
+//        // with assertThat
+//        assertThat($resultBoolean, equalTo(false));
+//    }
+//
+//    public function testAssertNull(): void
+//    {
+//        $resultX = null;
+//        $this->assertNull($resultX, "should be null");
+//        $this->assertNull($resultX);
+//        // with assertThat
+//        assertThat($resultX, nullValue());
+//    }
+//
+//    public function testAssertNotNull(): void
+//    {
+//        $resultX = new X(null);
+//        $this->assertNotNull($resultX, "should be not null");
+//        $this->assertNotNull($resultX);
+//        // with assertThat
+//        assertThat($resultX, not(nullValue()));
+//        assertThat($resultX, notNullValue());
+//    }
+//
+//    public function testAssertSame(): void
+//    {
+//        $expectedX = new X(null);
+//        $resultX = $expectedX;
+//        $this->assertSame($expectedX, $resultX, "should be same");
+//        $this->assertSame($expectedX, $resultX);
+//        // better than
+//        assertThat($resultX, sameInstance($resultX));
+//    }
+//
+//    public function testAssertNotSame(): void
+//    {
+//        $expectedX = new X(null);
+//        $resultX = new X(null);
+//        $this->assertNotSame($expectedX, $resultX, "should be distinct");
+//        $this->assertNotSame($expectedX, $resultX);
+//        // better than
+//        assertThat($resultX, not(sameInstance($expectedX)));
+//    }
+
+test('assertArrayEquals', () => {
+  const expectedArray = [1, 2, 3]
+  const resultArray = [1, 2, 3]
+  expect(expectedArray).toEqual(resultArray)
+
+  const expectedXArray = [new X(null), new X(null), new X(null)]
+  const resultXArray = [expectedXArray[0], expectedXArray[1], expectedXArray[2]]
+
+  expect(expectedXArray).toEqual(resultXArray)
+})
+
+test('assertThatForRelational', () => {
+  const resultInt = 1
+  expect(resultInt)
+    .toBeGreaterThan(0)
+    .toBeGreaterThanOrEqual(0)
+    .toBeGreaterThanOrEqual(1)
+    .toBeLessThan(2)
+    .toBeLessThanOrEqual(2)
+    .toBeLessThanOrEqual(1)
+})
+//
+//    public function testAssertThatForRelational(): void
+//    {
+//        $resultInt = 1;
+//        assertThat($resultInt, greaterThan(0));
+//        assertThat($resultInt, greaterThanOrEqualTo(0));
+//        assertThat($resultInt, greaterThanOrEqualTo(1));
+//        assertThat($resultInt, lessThan(2));
+//        assertThat($resultInt, lessThanOrEqualTo(2));
+//        assertThat($resultInt, lessThanOrEqualTo(1));
+//    }
+//
+//    public function testAssertThatCombined(): void
+//    {
+//        assertThat("characters", allOf(startsWith("char"), endsWith("ters")));
+//        assertThat("characters", anyOf(containsString("car"), containsString("rac")));
+//    }
+//
+//    public function testAssertThatArray(): void
+//    {
+//        $resultList = [1, 2, 3];
+//        assertThat($resultList, hasItemInArray(2));
+//        assertThat($resultList, arrayContainingInAnyOrder(1, 3, 2));
+//    }
+//}
